d4cec508f2dedd55fa2e703bb4ede18b1cebc56a
Braulio J. S. Santos <braulio.j.s.santos@gmail.com>
2020-08-06 18:52:33 -0300

    Efetuado criação de rota para busca de agendamento por nome

--- core/views.py
+++ core/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from core.models import Evento
 
-# Create your views here.
+def TituloEvento (request, titulo_evento):
+    consulta = Evento.objects.get(titulo=titulo_evento)
+    titulo = consulta.titulo
+    data_evento = consulta.data_evento
+    return HttpResponse('Evento: {} <br>Data: {}'.format(titulo, data_evento))
+
